ee3304aa4dca730853feb355c1e67825bbaf4bbc
TanushKrashak <tankrashak38816@gmail.com>
2024-06-02 20:46:12 +0530

    Created New Actor Cpp_BuildingVisual

---
+++ Source/Cpp_BasicBaseBuilder/Private/Actors/Cpp_BuildingVisual.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Cpp_BuildingVisual.h"
+
+
+ACpp_BuildingVisual::ACpp_BuildingVisual() {
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void ACpp_BuildingVisual::BeginPlay() {
+	Super::BeginPlay();
+	
+}
+void ACpp_BuildingVisual::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+}
+
