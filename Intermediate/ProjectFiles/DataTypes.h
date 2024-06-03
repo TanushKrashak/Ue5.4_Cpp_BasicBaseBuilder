a3b91a7a9a325e3b578151e8a89ad0f2ee29bfe7
TanushKrashak <tankrashak38816@gmail.com>
2024-06-03 22:08:36 +0530

    Deleted DataTypes.h From Project

--- Intermediate/ProjectFiles/DataTypes.h
+++ Intermediate/ProjectFiles/DataTypes.h
@@ -1,21 +1,2 @@
-#pragma once
 
-#include "DataTypes.generated.h"
-
-UENUM(BlueprintType)
-enum class EBuildType : uint8 {
-	Foundation	UMETA(DisplayName = "Foundation"),
-	Wall		UMETA(DisplayName = "Wall"),
-};
-
-USTRUCT(BlueprintType)
-struct FBuildingVisualType {
-	GENERATED_BODY()
-
-	UPROPERTY(EditDefaultsOnly, Category = "Building")
-	class UStaticMesh* BuildingMesh;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Building")
-	EBuildType BuildType;
-};
 
